1c1d03c65cd5e9471243bf635dc8b56b2f544e59
jhonpraxedesumg <jsalazarz@miumg.edu.gt>
2024-04-20 23:02:48 -0600

    MONGODB

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import org.example.crud.ClsTelefonos;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        ClsTelefonos tel = new ClsTelefonos();
+        tel.conexion();
+        tel.agregarTelefono();
     }
 }
--- src/main/java/org/example/crud/ClsTelefonos.java
+++ src/main/java/org/example/crud/ClsTelefonos.java
@@ -1,53 +1,59 @@
 package org.example.crud;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
 
-import javax.swing.text.Document;
+import org.bson.Document;
+
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClsTelefonos {
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> collection;
+
+    public void conexion() {
+        //cadena de conexion, contiene la información de la insalacion del mongodb
+        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
+
+        //se crean las configuraciones especificas para conexion y manejo de la base de datos
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        mongoClient = MongoClients.create(settings);
+        database = mongoClient.getDatabase("miBasedatos");
+        collection = database.getCollection("telefono");
+        System.out.println("conexion realizada");
+
+    }
+
     public void agregarTelefono(){
         TelefonoDTO telefono= new TelefonoDTO();
-        telefono.setNombre("Monica");
-        telefono.setCorreo("Monica@example.com");
-        telefono.setIdTelegram(123456789);
-        telefono.
 
-        Document document = new Document("Nombre",usuario.getCorreo())
-                .append("correo",usuario.getCorreo())
-                .append("IdTelegram",usuario.getIdTelegram());
-        collection.insertOne(document);
-        System.out.println("Usuario Creado");
-    }
-    public void leerUsuario(){
-        List<mdUsuario> usuarios=new ArrayList<>();
-        for (Document doc: collection.find()){
-            mdUsuario u = new mdUsuario();
-            u.setIdTelegram(doc.getLong("idTelegram"));
-            u.setNombre(doc.getString("nombre"));
-            u.setCorreo(doc.getString("correo"));
-            usuarios.add(u);
+        telefono.setMarca(JOptionPane.showInputDialog("ingrese marca ="));
+        telefono.setModelo(JOptionPane.showInputDialog("ingrese modelo ="));
+        telefono.setSisOperativo(JOptionPane.showInputDialog("ingrese SO ="));
+
+//        Document documento = new Document("marca",telefono.getMarca())
+//                .append("modelo",telefono.setModelo())
+//                .append("sisOperativo",telefono.getSisOperativo());
+        Document documento = new Document("marca",telefono.getMarca())
+                .append("modelo",telefono.getModelo())
+                .append("sisOperativo",telefono.getSisOperativo());
+
+        collection.insertOne(documento);
+        System.out.println("usuario creado");
         }
-        for (mdUsuario u: usuarios){
-            System.out.println("usuario:"+u.getNombre()+"correo:"= u.getCorreo());
-        }
-    }
-    private  void actualizarUsuario() {
-        collection.updateOne(Filters.eq("idTelegram", 88745587),
-                Updates.set("correo", "Nuevo@example.com"));
-        System.out.println("Usuario actualizado");
+
+
     }
 
-    private  void eliminarUsuario() {
-        collection.deleteOne(Filters.eq("idTelegram", 9995587));
-        System.out.println("Usuario eliminado");
-    }
 
-    private  void desconectarMongoDB() {
-        // Cerrar la conexión
-        mongoClient.close();
-        System.out.println("Desconectado de MongoDB");
-    }
-}
